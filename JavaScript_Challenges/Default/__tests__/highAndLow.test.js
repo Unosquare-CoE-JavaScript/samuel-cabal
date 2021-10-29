ac263b05813b960c3119a7e513585eec4d9fad13
scabal-00 <samuel.cabal@unosquare.com>
2021-10-29 12:47:16 -0500

    add 2 challenges of codewars. Validate Pin with regular expressions, Song decoder wit also regular expressions and an update for Code Morse

--- JavaScript_Challenges/Default/__tests__/highAndLow.test.js
+++ JavaScript_Challenges/Default/__tests__/highAndLow.test.js
@@ -4,25 +4,25 @@ describe("Highest and Lowest", () => {
   it("Validating with a string of numbers", () => {
     expect(highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4")).toBe("42 -9");
   });
-  it("", () => {
+  it("Validating a normal string of numbers", () => {
     expect(highAndLow("1 2 3 4 5")).toBe("5 1");
   });
-  it("", () => {
+  it("Validating a string with negative numbers", () => {
     expect(highAndLow("1 2 -3 4 5")).toBe("5 -3");
   });
-  it("", () => {
+  it("Validating other string with negative numbers", () => {
     expect(highAndLow("1 9 3 4 -5")).toBe("9 -5");
   });
-  it("", () => {
+  it("Validating an empty string", () => {
     expect(highAndLow(" ")).toBe("0 0");
   });
-  it("", () => {
+  it("Validating another empty string", () => {
     expect(highAndLow("")).toBe("0 0");
   });
-  it("", () => {
+  it("Validating a string with the same values", () => {
     expect(highAndLow("0 0 0 0")).toBe("0 0");
   });
-  it("", () => {
+  it("Validating other string with negative numbers", () => {
     expect(highAndLow("10 0 10 -5 7 -5")).toBe("10 -5");
   });
 });
